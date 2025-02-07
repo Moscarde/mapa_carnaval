a818fe89ec76122237a648c5b86548c24061d28f
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-06 22:25:41 -0300

    feat: enhance CarnavalMapView to include event date and city filtering

--- carnaval_map/views.py
+++ carnaval_map/views.py
@@ -11,11 +11,18 @@ class CarnavalMapView(View):
             "name",
             "city",
             "subtitle",
+            "event_date",
+            "event_day",
+            "event_time",
             "description",
             "ticket_info",
             "ticket_url",
             "latitude",
             "longitude",
         )
-        context = {"blocos": list(blocos)}
+        cities = set(bloco["city"].replace("-", " ").title() for bloco in blocos)
+
+        dates = set(bloco["event_date"] for bloco in blocos)
+
+        context = {"blocos": list(blocos), "cities": list(cities), "dates": list(dates)}
         return render(request, "carnaval_map/index.html", context)
