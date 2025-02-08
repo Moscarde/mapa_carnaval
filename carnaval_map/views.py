928ac992d76c726212597a0cf75b2e9ea44cd5c0
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-07 22:24:26 -0300

    feat: add neighborhood field to context in CarnavalMapView and FilterBlocosView

--- carnaval_map/views.py
+++ carnaval_map/views.py
@@ -1,7 +1,8 @@
+from datetime import datetime
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import JsonResponse
-from datetime import datetime
 
 from .models import Bloco, City
 
@@ -21,6 +22,7 @@ class CarnavalMapView(View):
             "ticket_url",
             "latitude",
             "longitude",
+            "neighborhood",
         )
 
         cities_coords = City.objects.values("name", "avg_latitude", "avg_longitude")
@@ -28,9 +30,11 @@ class CarnavalMapView(View):
         cities_ids = set(bloco["city"] for bloco in blocos)
         cities = [(city, city.replace("-", " ").title()) for city in cities_ids]
 
-        dates = set(bloco["event_date"] for bloco in blocos) # preciso mudar isso
+        dates = set(bloco["event_date"] for bloco in blocos)  # preciso mudar isso
         dates = sorted(dates)
-        dates = [(date.strftime("%Y-%m-%d"), date.strftime("%d/%m/%Y")) for date in dates]
+        dates = [
+            (date.strftime("%Y-%m-%d"), date.strftime("%d/%m/%Y")) for date in dates
+        ]
 
         context = {
             "blocos": list(blocos),
@@ -40,8 +44,10 @@ class CarnavalMapView(View):
         }
         return render(request, "carnaval_map/index.html", context)
 
-from django.http import JsonResponse
+
 from django.db.models import F
+from django.http import JsonResponse
+
 
 class FilterBlocosView(View):
     def get(self, request):
@@ -61,16 +67,17 @@ class FilterBlocosView(View):
                 "city",
                 "subtitle",
                 "event_date",
+                "event_day",
+                "event_time",
                 "description",
                 "latitude",
                 "longitude",
+                "neighborhood",
             )
         )
 
         # Pegando apenas as datas disponíveis na cidade selecionada
-        dates = sorted(
-            blocos_query.values_list("event_date", flat=True).distinct()
-        )
+        dates = sorted(blocos_query.values_list("event_date", flat=True).distinct())
         dates = [(date, date.strftime("%d/%m/%Y")) for date in dates]
 
         return JsonResponse({"blocos": blocos, "dates": dates})
