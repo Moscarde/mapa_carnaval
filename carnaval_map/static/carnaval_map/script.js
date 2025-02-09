6b067dfbe51bd4f1a272133a8fbf015708a1de3c
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-02-09 17:08:26 -0300

    feat: improve map marker popups and enhance CSS styling for better user experience

--- carnaval_map/static/carnaval_map/script.js
+++ carnaval_map/static/carnaval_map/script.js
@@ -1,80 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
 	function createMap(center, zoom) {
-        const map = L.map("map").setView(center, zoom);
-        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+		const map = L.map("map").setView(center, zoom);
+		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
 
-        // Legenda para os ícones
-        const legend = L.control({ position: "bottomright" });
+		// Legenda para os ícones
+		const legend = L.control({ position: "bottomright" });
 
-        legend.onAdd = function () {
-            const div = L.DomUtil.create("div", "info legend");
+		legend.onAdd = function () {
+			const div = L.DomUtil.create("div", "info legend");
 
-            // Estilo para a legenda
-            div.style.backgroundColor = "white";
-            div.style.padding = "10px";
-            div.style.borderRadius = "5px";
-            div.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.2)";
-            div.style.fontSize = "14px";
-            div.style.color = "black"
+			// Estilo para a legenda
+			div.style.backgroundColor = "white";
+			div.style.padding = "10px";
+			div.style.borderRadius = "5px";
+			div.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.2)";
+			div.style.fontSize = "14px";
+			div.style.color = "black";
 
-            // Definindo as cores e os ícones para a legenda
-            const categories = [
-                { color: "#71AE26", icon: "fa-star", label: "Gratuito - Futuro" },
-                { color: "#71AE26", icon: "fa-history", label: "Gratuito - Passado" },
-                { color: "orange", icon: "fa-star", label: "Pago - Futuro" },
-                { color: "orange", icon: "fa-history", label: "Pago - Passado" }
-            ];
+			// Definindo as cores e os ícones para a legenda
+			const categories = [
+				{ color: "#71AE26", icon: "fa-star", label: "Gratuito - Futuro" },
+				{ color: "#71AE26", icon: "fa-history", label: "Gratuito - Passado" },
+				{ color: "orange", icon: "fa-star", label: "Pago - Futuro" },
+				{ color: "orange", icon: "fa-history", label: "Pago - Passado" }
+			];
 
-            categories.forEach((category) => {
-                div.innerHTML += `
+			categories.forEach((category) => {
+				div.innerHTML += `
                     <div style="display: flex; align-items: center; margin-bottom: 8px;">
                         <i class="fa ${category.icon}" style="color: white; background-color: ${category.color}; border-radius: 50%; padding: 5px;"></i>
                         <span style="margin-left: 8px;">${category.label}</span>
                     </div>
                 `;
-            });
+			});
 
-            return div;
-        };
+			return div;
+		};
 
-        legend.addTo(map);
-        return map;
-    }
+		legend.addTo(map);
+		return map;
+	}
 
 	const freeUpcomingIcon = L.AwesomeMarkers.icon({
-        markerColor: "green",
-        iconColor: "white",
-        icon: "star",
-        prefix: "fa",
-        extraClasses: "fa-rotate-0"
-    });
-    
-    const freePastIcon = L.AwesomeMarkers.icon({
-        markerColor: "green",
-        iconColor: "white",
-        icon: "history",
-        prefix: "fa",
-        extraClasses: "fa-rotate-0"
-    });
-    
-    const paidUpcomingIcon = L.AwesomeMarkers.icon({
-        markerColor: "orange",
-        iconColor: "white",
-        icon: "star",
-        prefix: "fa",
-        extraClasses: "fa-rotate-0"
-    });
-    
-    const paidPastIcon = L.AwesomeMarkers.icon({
-        markerColor: "orange",
-        iconColor: "white",
-        icon: "history",
-        prefix: "fa",
-        extraClasses: "fa-rotate-0"
-    });
+		markerColor: "green",
+		iconColor: "white",
+		icon: "star",
+		prefix: "fa",
+		extraClasses: "fa-rotate-0"
+	});
+
+	const freePastIcon = L.AwesomeMarkers.icon({
+		markerColor: "green",
+		iconColor: "white",
+		icon: "history",
+		prefix: "fa",
+		extraClasses: "fa-rotate-0"
+	});
+
+	const paidUpcomingIcon = L.AwesomeMarkers.icon({
+		markerColor: "orange",
+		iconColor: "white",
+		icon: "star",
+		prefix: "fa",
+		extraClasses: "fa-rotate-0"
+	});
+
+	const paidPastIcon = L.AwesomeMarkers.icon({
+		markerColor: "orange",
+		iconColor: "white",
+		icon: "history",
+		prefix: "fa",
+		extraClasses: "fa-rotate-0"
+	});
 
 	const map = createMap([-22.85, -43.28], 12);
 	const cities_coords = JSON.parse(document.getElementById("cities_coords").textContent);
@@ -106,35 +106,64 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	function updateMarkers(blocos) {
-        clearMarkers();
-        const filteredBlocos = filterBlocos(); // Filtra os blocos conforme o checkbox
-    
-        filteredBlocos.forEach((bloco) => {
-            // Verifica se o bloco é gratuito ou pago
-            const isFree = bloco.ticket_info === "Grátis";
-            const today = new Date();
-            const blocoDate = new Date(bloco.event_date);
-            const isFuture = blocoDate >= today;
-            
-            // Escolher o ícone com base no tipo de bloco (grátis/pago) e no evento (futuro/passado)
-            let markerIcon;
-            if (isFree) {
-                // Bloco gratuito
-                markerIcon = isFuture ? freeUpcomingIcon : freePastIcon;
-            } else {
-                // Bloco pago
-                markerIcon = isFuture ? paidUpcomingIcon : paidPastIcon;
-            }
-    
-            // Criar o marcador com o ícone correto
-            const marker = L.marker([bloco.latitude, bloco.longitude], { icon: markerIcon }).addTo(map);
-            marker.bindPopup(`
-                <h3>${bloco.name}</h3>
-                <p>${bloco.description}</p>
+		clearMarkers();
+		const filteredBlocos = filterBlocos(); // Filtra os blocos conforme o checkbox
+
+		filteredBlocos.forEach((bloco) => {
+			// Verifica se o bloco é gratuito ou pago
+			const isFree = bloco.ticket_info === "Grátis";
+			const today = new Date();
+			const blocoDate = new Date(bloco.event_date);
+			const isFuture = blocoDate >= today;
+
+			// Escolher o ícone com base no tipo de bloco (grátis/pago) e no evento (futuro/passado)
+			let markerIcon;
+			if (isFree) {
+				// Bloco gratuito
+				markerIcon = isFuture ? freeUpcomingIcon : freePastIcon;
+			} else {
+				// Bloco pago
+				markerIcon = isFuture ? paidUpcomingIcon : paidPastIcon;
+			}
+
+			// Criar o marcador com o ícone correto
+			const marker = L.marker([bloco.latitude, bloco.longitude], { icon: markerIcon }).addTo(map);
+
+			const eventDate = new Date(bloco.event_date);
+			const formattedDate = eventDate.toLocaleDateString("pt-BR", {
+				day: "2-digit",
+				month: "2-digit"
+			});
+
+			ticket_content = "";
+			if (bloco.ticket_url) {
+				ticket_content = `💲 <a href="${bloco.ticket_url}" target="_blank">${bloco.ticket_info}</a>`;
+			} else {
+				ticket_content = `<strong>💲 ${bloco.ticket_info}</strong>`;
+			}
+
+			see_more_content = `🔍 <a href="${bloco.event_page_url || "#"}" target="_blank">Ver mais...</a>`;
+
+			map_link_content = `🗺️ <a href="${bloco.address_gmaps_url || "#"}" target="_blank">Veja como chegar</a>`;
+
+			whatsapp_text = `${formattedDate} - ${bloco.event_time} Vai acontecer o bloco ${bloco.name} em ${bloco.neighborhood || bloco.city}. ${bloco.event_page_url}`;
+			share_whatsapp_content = `📱 <a href="https://wa.me/?text=${whatsapp_text}" target="_blank">Compartilhar no WhatsApp</a>`;
+
+			marker.bindPopup(`
+                <div class="marker-popup">
+                    <h3>${bloco.name}</h3>
+
+                    <p>${ticket_content}</p>
+                    <p>📅 ${formattedDate} ${bloco.event_time}</p>
+                    <p>📍 ${bloco.neighborhood || bloco.city} - ${bloco.address || ""}</p>
+                    <p class="mt-2">${bloco.description.slice(0, 300) + (bloco.description.length > 300 ? "..." : "")}</p>
+
+                    <p class="mt-2">${see_more_content} ${map_link_content} ${share_whatsapp_content}</p>
+                </div>
             `);
-            markers.push(marker);
-        });
-    }
+			markers.push(marker);
+		});
+	}
 
 	function renderBlocos() {
 		const blocosContainer = document.getElementById("blocos");
